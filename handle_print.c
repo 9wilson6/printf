1f0d3a2f1ba5a8f1a2a6fa801e26b82891b64308
ANTH0RNY <nyaganthony@gmail.com>
2022-11-23 15:53:20 +0300

    fixed compilier issues

--- handle_print.c
+++ handle_print.c
@@ -15,7 +15,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],int flag
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent}
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
@@ -30,9 +30,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],int flag
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_len += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", -1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
